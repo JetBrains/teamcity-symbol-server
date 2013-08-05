eafac2a82ff7f40cbd76d0b99748bebb8f69fe40
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2013-08-05 14:40:39 +0400

    edit build feature parameters page added

---
+++ server/resources/editSymbolsBuildFeatureParams.jsp
@@ -0,0 +1,2 @@
+<%@ include file="/include-internal.jsp"%>
+<em>Symbols and sources indexing will be performed for all symbol files (.pdb) appeared among build artifacts.</em>
--- server/src/jetbrains/buildServer/symbols/IndexSymbolsBuildFeature.java
+++ server/src/jetbrains/buildServer/symbols/IndexSymbolsBuildFeature.java
@@ -1,13 +1,36 @@
 package jetbrains.buildServer.symbols;
 
+import jetbrains.buildServer.controllers.BaseController;
 import jetbrains.buildServer.serverSide.BuildFeature;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import jetbrains.buildServer.web.openapi.WebControllerManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Evgeniy.Koshkin
  */
 public class IndexSymbolsBuildFeature extends BuildFeature {
+
+  private String myEditParametersUrl;
+
+  public IndexSymbolsBuildFeature(PluginDescriptor pluginDescriptor, WebControllerManager web) {
+    final String jsp = pluginDescriptor.getPluginResourcesPath("editSymbolsBuildFeatureParams.jsp");
+    final String html = pluginDescriptor.getPluginResourcesPath("symbolIndexerSettings.html");
+
+    web.registerController(html, new BaseController() {
+      @Override
+      protected ModelAndView doHandle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        return new ModelAndView(jsp);
+      }
+    });
+
+    myEditParametersUrl = html;
+  }
 
   @NotNull
   @Override
@@ -18,12 +41,17 @@ public class IndexSymbolsBuildFeature extends BuildFeature {
   @NotNull
   @Override
   public String getDisplayName() {
-    return "Symbol files (.pdb) indexer";
+    return "Symbol Files Indexer";
   }
 
   @Nullable
   @Override
   public String getEditParametersUrl() {
-    return null;
+    return myEditParametersUrl;
+  }
+
+  @Override
+  public boolean isMultipleFeaturesPerBuildTypeAllowed() {
+    return false;
   }
 }
