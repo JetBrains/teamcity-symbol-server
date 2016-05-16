cfc19b3c6f90992fbac6b2e943bfd78e6381ec3c
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2016-05-16 19:40:51 +0300

    restored jet-symbols native tool sources

---
+++ jet-symbols/src/JetBrains.CommandLine.Symbols/ICommand.cs
@@ -0,0 +1,13 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: JetBrains.CommandLine.Symbols.ICommand
+// Assembly: JetBrains.CommandLine.Symbols, Version=1.0.0.0, Culture=neutral, PublicKeyToken=1010a0d8d6380325
+// MVID: EF046BF6-60AC-48EA-9121-8AF3D8D08853
+// Assembly location: C:\Data\Work\TeamCity\misc\tc-symbol-server\tools\JetSymbols\JetBrains.CommandLine.Symbols.exe
+
+namespace JetBrains.CommandLine.Symbols
+{
+  internal interface ICommand
+  {
+    int Execute();
+  }
+}
