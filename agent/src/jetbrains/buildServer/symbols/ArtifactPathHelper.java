12ce6a27133812a2b3efbc907a61fca399451ab6
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2016-05-18 19:30:03 +0300

    cleanup code after merge

--- agent/src/jetbrains/buildServer/symbols/ArtifactPathHelper.java
+++ agent/src/jetbrains/buildServer/symbols/ArtifactPathHelper.java
@@ -1,9 +1,7 @@
 package jetbrains.buildServer.symbols;
 
-import jetbrains.buildServer.ExtensionHolder;
-import jetbrains.buildServer.agent.impl.artifacts.ArchivePreprocessor;
-import jetbrains.buildServer.util.CollectionsUtil;
-import jetbrains.buildServer.util.filters.Filter;
+import jetbrains.buildServer.util.ArchiveType;
+import jetbrains.buildServer.util.ArchiveUtil;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -14,25 +12,14 @@ public class ArtifactPathHelper {
     private static final String ARCHIVE_PATH_SEPARATOR_FULL = "!/";
     private static final String FOLDER_SEPARATOR = "/";
 
-    private final ExtensionHolder myExtensions;
-
-    public ArtifactPathHelper(@NotNull final ExtensionHolder extensions) {
-        myExtensions = extensions;
-    }
-
     @NotNull
-    String concatenateArtifactPath(String fileNamePrefix, String pdbFileName) {
+    static String concatenateArtifactPath(String fileNamePrefix, String pdbFileName) {
       final String normilizedFileNamePrefix = fileNamePrefix.replace(ARCHIVE_PATH_SEPARATOR, ARCHIVE_PATH_SEPARATOR_FULL);
       final String delimiter = (isPathToArchive(normilizedFileNamePrefix) && !normilizedFileNamePrefix.contains(ARCHIVE_PATH_SEPARATOR)) ? ARCHIVE_PATH_SEPARATOR_FULL : FOLDER_SEPARATOR;
         return normilizedFileNamePrefix + delimiter + pdbFileName;
     }
 
-    private boolean isPathToArchive(@NotNull final String path){
-        return CollectionsUtil.contains(myExtensions.getExtensions(ArchivePreprocessor.class), new Filter<ArchivePreprocessor>() {
-            @Override
-            public boolean accept(@NotNull ArchivePreprocessor data) {
-                return data.shouldProcess(path);
-            }
-        });
+    static private boolean isPathToArchive(@NotNull final String path){
+        return ArchiveUtil.getArchiveType(path) != ArchiveType.NOT_ARCHIVE;
     }
 }
