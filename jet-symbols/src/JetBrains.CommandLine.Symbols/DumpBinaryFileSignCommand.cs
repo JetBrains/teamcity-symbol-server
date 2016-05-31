b67ad926c2e606934d4de91765c5b8ee186840e0
Evgeniy.Koshkin <evgeniy.koshkin@jetbrains.com>
2016-05-31 15:45:22 +0300

    - stop using C# v.Last features - cleanup code (cherry picked from commit 9e68c58)

--- jet-symbols/src/JetBrains.CommandLine.Symbols/DumpBinaryFileSignCommand.cs
+++ jet-symbols/src/JetBrains.CommandLine.Symbols/DumpBinaryFileSignCommand.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: JetBrains.CommandLine.Symbols.DumpBinaryFileSignCommand
-// Assembly: JetBrains.CommandLine.Symbols, Version=1.0.0.0, Culture=neutral, PublicKeyToken=1010a0d8d6380325
-// MVID: EF046BF6-60AC-48EA-9121-8AF3D8D08853
-// Assembly location: C:\Data\Work\TeamCity\misc\tc-symbol-server\tools\JetSymbols\JetBrains.CommandLine.Symbols.exe
-
-using JetBrains.Metadata.Utils;
+﻿using JetBrains.Metadata.Utils;
 using JetBrains.Metadata.Utils.PE;
 using JetBrains.Util;
 using System;
