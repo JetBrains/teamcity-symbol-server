86efeffe34781504bcb029034f29b7c0578de801
Evgeniy.Koshkin <evgeniy.koshkin@jetbrains.com>
2017-04-24 13:00:04 +0300

    fix compilation

--- server-tests/src/jetbrains/buildServer/symbols/MetadataStorageMock.java
+++ server-tests/src/jetbrains/buildServer/symbols/MetadataStorageMock.java
@@ -6,12 +6,13 @@ import jetbrains.buildServer.serverSide.metadata.MetadataStorageWriter;
 import jetbrains.buildServer.serverSide.metadata.impl.MetadataStorageException;
 import jetbrains.buildServer.util.Action;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
 class MetadataStorageMock implements MetadataStorage {
 
-  private List<BuildMetadataEntry> myEntries = new ArrayList<BuildMetadataEntry>();
+  private List<BuildMetadataEntry> myEntries = new ArrayList<>();
 
   public void addEntry(final long buildId, final String filePath, final String fileSignature) {
     myEntries.add(new BuildMetadataEntry() {
@@ -56,8 +57,47 @@ class MetadataStorageMock implements MetadataStorage {
     return false;
   }
 
+  @Override
+  public boolean removeBuildEntries(final long buildId, @NotNull String providerId) {
+    return myEntries.removeIf(buildMetadataEntry -> buildMetadataEntry.getBuildId() == buildId);
+  }
+
+  @Override
+  public void addBuildEntry(long l, @NotNull String s, @NotNull String s1, @NotNull Map<String, String> map, boolean b) {
+    myEntries.add(new BuildMetadataEntry() {
+      @Override
+      public long getBuildId() {
+        return l;
+      }
+
+      @NotNull
+      @Override
+      public String getKey() {
+        return s1;
+      }
+
+      @NotNull
+      @Override
+      public Map<String, String> getMetadata() {
+        return map;
+      }
+    });
+  }
+
   @NotNull
   public Iterator<BuildMetadataEntry> findEntriesWithValue(@NotNull String s, @NotNull String s1, Collection<String> collection) {
+    return myEntries.iterator();
+  }
+
+  @NotNull
+  @Override
+  public Iterator<BuildMetadataEntry> findEntriesWithValue(@NotNull String s, @NotNull String s1, @Nullable Collection<String> collection, boolean b) {
+    return myEntries.iterator();
+  }
+
+  @NotNull
+  @Override
+  public Iterator<BuildMetadataEntry> findEntriesWithKeyValuePairs(@NotNull String s, @NotNull Map<String, String> map, boolean b) {
     return myEntries.iterator();
   }
 }
